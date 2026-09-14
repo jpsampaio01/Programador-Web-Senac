c976e36a9d7a94ff398508962366e392aa92ad3f
João Paulo <jpsampaiolv@gmail.com>
2026-09-14 11:31:32 -0300

    att

---
+++ Programador-Web-Senac/Aula 12/atividade/script.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("Primeiro número:"));
+let num2 = Number(prompt("Segundo número"));
+console.log("Soma: " + (num1 + num2));
+console.log("Subtração: " + (num1 - num2));
+console.log("Multiplicação: " + (num1 * num2));
+console.log("Divisão: " + (num1 / num2));
---
+++ Programador-Web-Senac/Aula 12/script.js
@@ -0,0 +1,10 @@
+// console.log("Olá, mundo!");
+// prompt("Digite seu nome:");
+
+// let variavelString = "Sou uma string";
+// let variavelNumber = 10;
+// let variavelFalseNumber = "10";
+// let variavelBolean = true;
+
+let anoNascimento = prompt("Qual ano você nasceu? ")
+console.log("Idade é: "+(2026 - anoNascimento))
