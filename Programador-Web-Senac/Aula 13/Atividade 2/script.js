915ee462c9cb8112ecdc3bb4d5b44cd8fb0e14e6
João Paulo <jpsampaiolv@gmail.com>
2026-09-15 11:25:37 -0300

    att

---
+++ Programador-Web-Senac/Aula 13/Atividade 2/script.js
@@ -0,0 +1,4 @@
+let valor1 = Number(prompt("Digite um valor numérico: "));
+let valor2 = Number(prompt("Digite outro valor numérico: "));
+let divisao = valor1 / valor2;
+alert("O valor final da divisão é: " + divisao);
---
+++ Programador-Web-Senac/Aula 13/atividade 1/script.js
@@ -0,0 +1,5 @@
+let valor = Number(prompt("Digite um valor numérico: "));
+let desconto = valor * 0.15;
+let final = valor - desconto;
+console.log("Resultado: " + final);
+alert("Valor final com desconto: R$ " + final)
