f2fe23acbdb6a4e00553fda173eb30a07d21f725
João Paulo <jpsampaiolv@gmail.com>
2026-09-22 11:24:02 -0300

    att

--- Programador-Web-Senac/Aula 17/atividade notas/script.js
+++ Programador-Web-Senac/Aula 17/atividade notas/script.js
@@ -1,8 +1,8 @@
-function verificarMedia() {
+function calcularMedia() {
     let nota1 = Number(document.getElementById("nota1").value);
     let nota2 = Number(document.getElementById("nota2").value);
     let nota3 = Number(document.getElementById("nota3").value);
     let nota4 = Number(document.getElementById("nota4").value);
     let media = (nota1 + nota2 + nota3 + nota4) / 4
-    console.log("A média final é: " + media)
+    alert("A média final é: " + media)
 }
